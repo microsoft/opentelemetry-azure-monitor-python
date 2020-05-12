48bb46c6537fac17325a005872d6633535eeb08e
Leighton <lechen@microsoft.com>
2020-05-12 14:54:56 -0700

    update time

--- azure_monitor/src/azure_monitor/sdk/auto_collection/live_metrics/utils.py
+++ azure_monitor/src/azure_monitor/sdk/auto_collection/live_metrics/utils.py
@@ -1,11 +1,11 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 #
-import time
 import uuid
 
 from azure_monitor.protocol import LiveMetricEnvelope
 from azure_monitor.utils import azure_monitor_context
+from datetime import datetime
 
 DEFAULT_LIVEMETRICS_ENDPOINT = "https://rt.services.visualstudio.com"
 LIVE_METRICS_SUBSCRIBED_HEADER = "x-ms-qps-subscribed"
@@ -22,8 +22,11 @@ def create_metric_envelope(instrumentation_key: str):
         machine_name=azure_monitor_context.get("ai.device.id"),
         metrics=None,
         stream_id=STREAM_ID,
-        # TODO: Fix issue with incorrect time
-        timestamp="/Date({0})/".format(str(int(time.time()) * 1000)),
+        timestamp="/Date({0})/".format(str(calculate_ticks_since_epoch())),
         version=azure_monitor_context.get("ai.internal.sdkVersion"),
     )
     return envelope
+
+
+def calculate_ticks_since_epoch():
+    return (datetime.utcnow() - datetime(1, 1, 1)).total_seconds() * 10000000
