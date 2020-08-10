e326f1848c4ccae3c000ec241f17e989ccfb52bc
Leighton Chen <lechen@microsoft.com>
2020-08-10 13:01:45 -0700

    fix formatting

--- azure_monitor/src/azure_monitor/storage.py
+++ azure_monitor/src/azure_monitor/storage.py
@@ -200,10 +200,10 @@ class LocalFileStorage:
                     if size >= self.max_size:
                         logger.warning(
                             "Persistent storage max capacity has been "
-                            "reached. Currently at %fKB. Telemetry will be "
+                            "reached. Currently at {}KB. Telemetry will be "
                             "lost. Please consider increasing the value of "
-                            "'storage_max_size' in exporter config.",
-                            format(size / 1024),
+                            "'storage_max_size' in exporter config."
+                            .format(size/1024)
                         )
                         return False
         return True
