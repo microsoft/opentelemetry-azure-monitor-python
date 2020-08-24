2bef867b52eadfb9da24455c295eea133d96274c
Leighton Chen <lechen@microsoft.com>
2020-08-24 15:17:47 -0700

    lint

--- azure_monitor/src/azure_monitor/utils.py
+++ azure_monitor/src/azure_monitor/utils.py
@@ -58,7 +58,7 @@ class PeriodicTask(threading.Thread):
     """
 
     def __init__(self, interval, function, args=None, kwargs=None):
-        super(PeriodicTask).__init__()
+        super().__init__()
         self.interval = interval
         self.function = function
         self.args = args or []
